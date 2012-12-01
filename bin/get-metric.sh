3bb121d14529b40532dbd420e1dbf5c3831c2a5b
vlad.iovanov <vlad.iovanov@uhurusoftware.com>
2012-12-01 11:10:50 +0200

    Fixed nagios plugin script to work with RVM.
    Change-Id: Icac4a4628ea9e57a22734401ef114f7b8abecf97

--- bin/get-metric.sh
+++ bin/get-metric.sh
@@ -1,3 +1,2 @@
-source /home/vladi/.rvm/scripts/rvm
-
-ruby -v
+export rvm_path=/home/vladi/.rvm
+/home/vladi/.rvm/bin/rvm 1.9.2 do ruby /home/vladi/Desktop/code/private-uhuru-dashboard/lib/get_linux_stats.rb $1 $2 $3 $4 $5 $6
